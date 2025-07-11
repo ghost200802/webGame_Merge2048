46c2d72b29c1c2bd4eeb72f86e77dc1554a56020
Ghost <ghost200802@163.com>
2025-07-11 18:39:54 +0800

    init

---
+++ Assets/FMGames/Scripts/Ads.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+namespace FMGames {
+    public class Ads : MonoBehaviour {
+
+        // Use this for initialization
+        void Start() {
+#if UNITY_IOS
+        Advertisement.Initialize("4147408");
+#elif UNITY_ANDROID
+            Advertisement.Initialize("4147409");
+#endif
+
+        }
+
+        public float timeToShowAds = 180;
+        float timer;
+
+        // Update is called once per frame
+        void Update() {
+            if (timer > timeToShowAds) {
+                timer = 0;
+                //Advertisement.Show();
+            }
+        }
+    }
+}
